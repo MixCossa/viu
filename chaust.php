90bc1a1aee613b81147ccc9da44b70b7dbdb1759
MixCossa <43558170+MixCossa@users.noreply.github.com>
2018-09-29 04:28:40 +0000

    Create chaust.php

---
+++ chaust.php
@@ -0,0 +1,7 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = "99a3716f-05a8-4a61-9a5c-3ac64f1128cf";
+
+// kamu bisa membuat banyak config untuk beberapa akun. 
